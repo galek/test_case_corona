c803d720af9b16a2ff968ad73aed0d699e049073
Nick Galko <nikolay.galko@gmail.com>
2016-11-14 06:39:46 +0300

    Плавный переход на предыдущую сцену(по кнопке Back)

--- frm_2.lua
+++ frm_2.lua
@@ -5,61 +5,42 @@ local scene = composer.newScene()
 
 local mFile = require("FileFunctions")
 
-
 local mEditBox
+
+
+local function destroyEditBox()
+    if mEditBox then
+        mEditBox:removeSelf()
+        mEditBox = nil
+    end
+end
+
+local function goBack(event)
+    print("goBack")
+    composer.removeScene("frm_2")
+    -- composer.gotoScene( "frm_1" )
+    composer.gotoScene("frm_1", "crossFade", 1000)
+
+    destroyEditBox()
+
+    return true
+end
+
+local function onSave(event)
+    -- print("onSave")
+    -- print( event.target.id )  -- Reference to button's 'id' parameter
+
+    local filename = GetOpenedFile()
+
+    assert((not((filename == nil) or filename == "")), "INVALID FILENAME")
+    SaveFile(filename, mEditBox.text)
+
+    return true
+end
 
 -- Create scene
 function scene:create(event)
     local sceneGroup = self.view
-    local ox, oy = math.abs(display.screenOriginX), math.abs(display.screenOriginY)
-
-    -- Set color variables depending on theme
-    local tableViewColors = {
-        rowColor = { default = { 1 }, over = { 30 / 255, 144 / 255, 1 } },
-        lineColor = { 220 / 255 },
-        catColor = { default = { 150 / 255, 160 / 255, 180 / 255, 200 / 255 }, over = { 150 / 255, 160 / 255, 180 / 255, 200 / 255 } },
-        defaultLabelColor = { 0, 0, 0, 0.6 },
-        catLabelColor = { 0 }
-    }
-
-    tableViewColors.rowColor.default = { 48 / 255 }
-    tableViewColors.rowColor.over = { 72 / 255 }
-    tableViewColors.lineColor = { 36 / 255 }
-    tableViewColors.catColor.default = { 80 / 255, 80 / 255, 80 / 255, 0.9 }
-    tableViewColors.catColor.over = { 80 / 255, 80 / 255, 80 / 255, 0.9 }
-    tableViewColors.defaultLabelColor = { 1, 1, 1, 0.6 }
-    tableViewColors.catLabelColor = { 1 }
-
-    -- Forward reference for the tableView
-    local tableView
-
-    -- Text to show which item we selected
-    local itemSelected = display.newText("User selected row ", 0, 0, native.systemFont, 16)
-    itemSelected:setFillColor(unpack(tableViewColors.catLabelColor))
-    itemSelected.x = display.contentWidth + itemSelected.contentWidth
-    itemSelected.y = display.contentCenterY
-    sceneGroup:insert(itemSelected)
-
-    -- Function to return to the tableView
-    local function goBack(event)
-        transition.to(tableView, { x = display.contentWidth * 0.5, time = 600, transition = easing.outQuint })
-        transition.to(itemSelected, { x = display.contentWidth + itemSelected.contentWidth, time = 600, transition = easing.outQuint })
-        transition.to(event.target, { x = display.contentWidth + event.target.contentWidth, time = 480, transition = easing.outQuint })
-    end
-
-    -- Back button
-    local backButton = widget.newButton {
-        width = 128,
-        height = 32,
-        label = "back",
-        onRelease = goBack
-    }
-    backButton.x = display.contentWidth + backButton.contentWidth
-    backButton.y = itemSelected.y + itemSelected.contentHeight + 16
-    sceneGroup:insert(backButton)
-
-
-
     local function textListener(event)
 
         if (event.phase == "began") then
@@ -88,56 +69,51 @@ function scene:create(event)
     mEditBox.font = native.newFont("Helvetica-Bold", 18)
     mEditBox:addEventListener("userInput", textListener)
 
-end
+    sceneGroup:insert(mEditBox)
 
-local function goBack(event)
-    -- transition.to( tableView, { x=display.contentWidth*0.5, time=600, transition=easing.outQuint } )
-    -- transition.to( itemSelected, { x=display.contentWidth+itemSelected.contentWidth, time=600, transition=easing.outQuint } )
-   -- transition.to(event.target, { x = display.contentWidth + event.target.contentWidth, time = 480, transition = easing.outQuint })
-   
-   
-    local switch = event.target
-    print( "Switch with ID '"..switch.id.."' is on: "..tostring(switch.isOn) )
-   
-end
-
-local function onSave(event)
-    --print("onSave")
-    --print( event.target.id )  -- Reference to button's 'id' parameter
-    
-    local filename=GetOpenedFile()
-    
-    assert((not((filename==nil)or filename=="")),"INVALID FILENAME")    
-    SaveFile(filename,mEditBox.text)    
-end
-
--- create the actual tabBar widget
-
-local ox, oy = math.abs(display.screenOriginX), math.abs(display.screenOriginY)
-    
-local Save = widget.newButton(
+    Save = widget.newButton(
     {
         top = display.contentHeight - 50,
         width = 32,
         height = 32,
-        left = 300 + ox + ox, -- HARDCODED SIZE
+        left = 300 + ox + ox,
+        -- HARDCODED SIZE
         id = "button1",
         label = "Save",
-        onEvent = onSave
+        onRelease = onSave
     }
-)  
-local Back = widget.newButton(
+    )
+    sceneGroup:insert(Save)
+
+    Back = widget.newButton(
     {
         top = display.contentHeight - 50,
         width = 32,
         height = 32,
-        left = 20 + ox + ox, -- HARDCODED SIZE
+        left = 20 + ox + ox,
+        -- HARDCODED SIZE
         id = "button2",
         label = "Back",
-        onEvent = goBack
+        onRelease = goBack
     }
-)
+    )
+    sceneGroup:insert(Back)
+end
 
-scene:addEventListener("create")
+function scene:destroyScene(event)
+    local group = self.view
+
+    destroyEditBox()
+end
+
+function scene:exitScene(event)
+    local group = self.view
+
+    destroyEditBox()
+end
+
+scene:addEventListener("destroyScene", scene)
+scene:addEventListener("exitScene", scene)
+scene:addEventListener("create", scene)
 
 return scene
